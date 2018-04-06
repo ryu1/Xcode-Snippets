da3e4b65fcd70066430985747d6fc1164b9b33f1
Ryuichi Ishitsuka <jeremy.hillary.boob@gmail.com>
2018-04-06 17:54:20 +0900

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #ln -s ../Xcode-Snippets ~/Library/Developer/Xcode/UserData/CodeSnippets
-cd ~/Library/Developer/Xcode/UserData
-ln -s ~/git/Xcode-Snippets  CodeSnippets
+#cd ~/Library/Developer/Xcode/UserData
+ln -s ~/Documents/git/Xcode-Snippets ~/Library/Developer/Xcode/UserData/CodeSnippets
